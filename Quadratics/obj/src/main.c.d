ef0b6ac4c5f556e008edfffcbf5784c42523e030
TI-Pi-Plus-C <huff-ether-upriver@duck.com>
2026-05-02 15:47:51 -0600

    Add files via upload

---
+++ Quadratics/obj/src/main.c.d
@@ -0,0 +1,12 @@
+obj/src/main.c.bc: src/main.c \
+  /home/james/CEdev-Linux/CEdev/include/ti/screen.h \
+  /home/james/CEdev-Linux/CEdev/include/stdbool.h \
+  /home/james/CEdev-Linux/CEdev/include/stdint.h \
+  /home/james/CEdev-Linux/CEdev/include/cdefs.h \
+  /home/james/CEdev-Linux/CEdev/include/stddef.h \
+  /home/james/CEdev-Linux/CEdev/include/ti/ui.h \
+  /home/james/CEdev-Linux/CEdev/include/ti/getcsc.h \
+  /home/james/CEdev-Linux/CEdev/include/ti/real.h \
+  /home/james/CEdev-Linux/CEdev/include/math.h \
+  /home/james/CEdev-Linux/CEdev/include/__math_def.h \
+  /home/james/CEdev-Linux/CEdev/include/string.h
